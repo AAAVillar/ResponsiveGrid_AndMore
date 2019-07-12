b1791a9ced523d8e97eaa74fecdda3a1ebd8aa44
Arnold Villar <furthereducation77@gmail.com>
2019-07-11 23:59:52 -0400

    Using Responsive tech

---
+++ main.js
@@ -0,0 +1,10 @@
+// connection
+
+
+
+function _(x){
+	return document.getElementById(x);
+};
+var myDate = new Date();
+
+_('demo1').innerHTML = "Today is " + myDate.toDateString() + "<sub> courtesy of javascript</sub>";
